e10f3c54983fb83bbfe6ec0340e5afe47dfa8cc2
Jon Coulter <jon@gocanvas.io>
2017-10-03 13:39:49 +0000

    changes

--- start.sh
+++ start.sh
@@ -6,4 +6,5 @@ set
 
 # cd stanford-corenlp-full-2015-12-09
 
-java -cp "*" -mx1g edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port $PORT
+#java -cp "*" -mx1g edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port $PORT
+java -cp "*" -Xmx2g -Dfile.encoding=UTF-8  edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port $PORT
